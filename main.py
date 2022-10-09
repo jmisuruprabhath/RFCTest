0e951e660936f729d9d400358a6ab96f314ae364
Isuru Prabhath <76735398+jmisuruprabhath@users.noreply.github.com>
2022-10-09 12:01:23 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import pickle
+import json
+
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class model_input(BaseModel):
+    
+    age : int
+    sex : int
+    bloodpressure : float
+    cholesterol : int
+    diabetes : int
+    smoking : int    
+
+# loading the saved model
+model = pickle.load(open('heartdisease_model.pkl','rb'))
+
+
+@app.post('/cardiac_prediction')
+def diabetes_pred(input_parameters : model_input):
+    
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+    
+    age = input_dictionary['age']
+    sex = input_dictionary['sex']
+    bp = input_dictionary['bloodpressure']
+    ch = input_dictionary['cholesterol']
+    db = input_dictionary['diabetes']
+    smk = input_dictionary['smoking']
+
+
+
+    input_list = [age, sex, bp, ch, db, smk]
+    
+    prediction = model.predict([input_list])
+    
+    if prediction[0] == 0:
+        return 'The person can not  '
+    
+    else:
+        return 'The person can '
+
+
